3982fcd9b52da00bdfee14ba0c02611edfc30eb8
mustafa1133 <mustafa1133@hotmail.com>
2019-04-21 14:41:04 -0400

    first commit

---
+++ Lab 1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle rectangle = new Rectangle();
+
+
+            Rectangle smallRectangle=Rectangle.CreateRectangle(4,5);
+            Rectangle.DescribeRectanlge(smallRectangle);
+            Rectangle.CalculateAndDisplayArea(smallRectangle);
+        }
+    }
+}
---
+++ Lab 1/Rectangle.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_1
+{
+    public class Rectangle
+    {
+        public int Width { get; set; }
+        public int Length { get; set; }
+
+        private int area;
+
+        public Rectangle(int w, int l)
+        {
+            Width=w;
+            Length=l;
+
+        }
+
+        public Rectangle()
+        {
+
+        }
+
+
+
+        public static Rectangle CreateRectangle (int width, int length)
+        {
+            Rectangle rec = new Rectangle(width,length);
+            return rec;
+
+        }
+
+
+        public static void CalculateAndDisplayArea(Rectangle rec)
+        {
+            rec.area = rec.Width * rec.Length;
+
+            Console.WriteLine($"the Area is {rec.area}");
+
+        }
+
+        public static void DescribeRectanlge(Rectangle rec)
+        {
+            Console.WriteLine($"The Width is {rec.Width} and the Length is {rec.Length}");
+        }
+    }
+}
